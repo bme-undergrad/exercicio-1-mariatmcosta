088e9d4cf11f3a7ee128fe4fd154fc3efb5dfe3c
mariatmcosta <dudatmcosta@icloud.com>
2025-08-27 09:41:56 -0300

    modifiquei t

--- exercicio1.m
+++ exercicio1.m
@@ -1,14 +1,25 @@
-function t = exercicio1(func,x0)
+function t = exercicio1(func,func_d,x0)
 
 % nao alterar: inicio
-es = 1;
+es = 0.01;
 imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
-t = 0; % alterar
+t_roots = zeros(imax,1);
+t_roots(1) = x0;
+
+for ii = 1:length(t_roots)-1
+  if ii ~= 1
+    erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii))
+    if erro(ii) < es
+      break
+    endif
+  endif
+    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii));
+endfor
+t = t_roots(ii); 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
