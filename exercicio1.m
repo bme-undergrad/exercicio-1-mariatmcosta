bc2c404c7ee12165cd5f0150f539d610043150cf
mariatmcosta <dudatmcosta@icloud.com>
2025-08-27 11:35:51 -0300

    modifiquei t

--- exercicio1.m
+++ exercicio1.m
@@ -9,28 +9,23 @@ imax = 20;
 
 % Segundo chute inicial, necessário para o método da secante:
   if x0 == 0
-    x1 = 1e-3;  % ou qualquer valor significativo
+    x1 = 1e-3;  
   else
     x1 = x0 * 1.1;
   endif
 
 
   t_roots = zeros(imax,1);
-  erro = zeros(imax,1);
-
   t_roots(1) = x0;
   t_roots(2) = x1;
 
   for ii = 2:imax
-    if ii ~= 1
-      erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii));
-      fprintf('Iteração %d: t = %.6f, erro = %.6f\n', ii, t_roots(ii), erro(ii));
-      if erro(ii) < es
-        break
-      endif
-    endif
-     % fórmula da secante
+    % fórmula da secante
     t_roots(ii+1) = t_roots(ii) - func(t_roots(ii)) * ((t_roots(ii) - t_roots(ii-1)) / (func(t_roots(ii)) - func(t_roots(ii-1))));
+    erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii));
+    if erro(ii) < es
+      break
+    endif
   endfor
   
   t = t_roots(ii);
