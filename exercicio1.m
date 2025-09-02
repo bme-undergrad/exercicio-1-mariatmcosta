85e899b305f897687f2b522bff6ce2034942f020
Maria Eduarda C <dudatmcosta@icloud.com>
2025-09-02 09:27:55 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,4 +1,4 @@
-function t = exercicio1(func,func_d,x0)
+function t = exercicio1(func,x0)
 
 % nao alterar: inicio
 es = 0.01;
@@ -7,19 +7,27 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-t_roots = zeros(imax,1);
-t_roots(1) = x0;
+% Segundo chute inicial, necessário para o método da secante:
+  if x0 == 0
+    x1 = 1e-3;  
+  else
+    x1 = x0 * 1.1;
+  endif
 
-for ii = 1:length(t_roots)-1
-  if ii ~= 1
-    erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii))
+  t_roots = zeros(imax,1);
+  t_roots(1) = x0;
+  t_roots(2) = x1;
+
+  for ii = 2:imax
+    % fórmula da secante
+    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii)) * ((t_roots(ii) - t_roots(ii-1)) / (func(t_roots(ii)) - func(t_roots(ii-1))));
+    erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii));
     if erro(ii) < es
       break
     endif
-  endif
-    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii));
-endfor
-t = t_roots(ii); 
+  endfor
+  
+  t = round(t_roots(ii) * 1e4) / 1e4;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
